daf69f602e6020a21825b6f397eb45c968082773
filemage1 <andrewk@filemage.io>
2020-06-05 16:07:15 -0700

    use a shared host key when load balancing

--- 202-high-availability-vm-database/files/initialize-application.sh
+++ 202-high-availability-vm-database/files/initialize-application.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -ex
 
+# Each instance needs to present the same host key when accessed
+# through the load balancer. For demo purposes we are going to
+# use a hardcoded key, in a production environment you should
+# generate a unique key.
+cp ./ssh_host_rsa_key /etc/filemage/ssh_host_rsa_key
+chmod 600 /etc/filemage/ssh_host_rsa_key
+
 # Write the the database connection info to the application config.
 cat > /etc/filemage/config.yml << EOF
 tls_certificate: /opt/filemage/default.cert
@@ -11,6 +18,8 @@ pg_database: filemage
 pg_ssl_mode: require
 ftp_data_port_start: 32768
 ftp_data_port_end: 60999
+sftp_host_keys:
+  - /etc/filemage/ssh_host_rsa_key
 EOF
 
 systemctl restart filemage
