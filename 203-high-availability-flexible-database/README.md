54f69dc5eced96297f3b6d353f7359427ab2fe78
whileloop <andrewk@filemage.io>
2022-04-27 20:30:57 -0700

    add template for postgres flexible database

---
+++ 203-high-availability-flexible-database/README.md
@@ -0,0 +1,22 @@
+# Deploy Load Balanced FileMage Gateway With Managed PostgreSQL
+
+This template provides a easy way to deploy a load balanced deployment of FileMage Gateway with a Azure managed database for PostgreSQL.
+
+The template will create a virtual network and subnet. If you want to deploy into a existing virtual network, remove that resource from the template and replace the variable `subnetId` with the ID of your existing subnet.
+
+The parameter `adminAccessIp` specifies your local workstation or office IP address. This is used to create a firewall rule to the managed database for administrative purposes.
+
+The parameter `adminCidr` specifies the CIDR address range of your local workstation or office. This is used to restrict SSH access.
+
+Once the deployment is complete use the DNS name from the output `loadbalancerDns` to access your deployment.
+
+[<img src="http://azuredeploy.net/deploybutton.png"/>](https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Ffilemage%2Fazure-quickstart-templates%2Fmaster%2F201-high-availability-managed-database%2Fazuredeploy.json)
+
+## Post Deployment
+
+After deploying the template, it is recommended to restart the database to apply the extension settings. This can be done in the Azure Portal or using the following commands:
+
+```
+DB_NAME=$(az deployment group show --resource-group YourResourceGroupName -n YourDeploymentName --query properties.outputs.dbName.value -otsv)
+az postgres flexible-server restart --resource-group YourResourceGroupName --name $DB_NAME
+```
---
+++ 203-high-availability-flexible-database/files/initialize-application.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -ex
+
+# Each instance needs to present the same host key when accessed
+# through the load balancer. For demo purposes we are going to
+# use a hardcoded key, in a production environment you should
+# generate a unique key.
+cp ./ssh_host_rsa_key /etc/filemage/ssh_host_rsa_key
+chmod 600 /etc/filemage/ssh_host_rsa_key
+
+# Need .pgpass to avoid password prompt.
+echo "$1:5432:filemage:$2:$3" > .pgpass
+chmod 600 .pgpass
+
+# Configure the pg_partman extension and associated schema.
+# Ideally this would only be done once but since these
+# commands are idempotent we can run this on each instance boot
+# to keep the automation simple.
+PGPASSFILE=.pgpass psql -h $1 -U $2 -d filemage << EOF
+CREATE SCHEMA IF NOT EXISTS partman;
+CREATE EXTENSION IF NOT EXISTS pg_partman SCHEMA partman;
+GRANT ALL ON SCHEMA partman TO filemage;
+GRANT ALL ON ALL TABLES IN SCHEMA partman TO filemage;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA partman TO filemage;
+GRANT EXECUTE ON ALL PROCEDURES IN SCHEMA partman TO filemage;
+EOF
+
+rm .pgpass
+
+# Write the database connection info to the application config.
+cat > /etc/filemage/config.yml << EOF
+tls_certificate: /opt/filemage/default.cert
+tls_certificate_key: /opt/filemage/default.key
+pg_host: $1
+pg_user: $2
+pg_password: $3
+pg_database: filemage
+pg_ssl_mode: require
+sftp_host_keys:
+  - /etc/filemage/ssh_host_rsa_key
+EOF
+
+# Write the session secret defined in the template to each
+# instance so cookies can be shared across instances.
+echo $4 > /opt/filemage/.secret
+
+systemctl restart filemage
+
+# Disable the database that comes pre-installed on the image.
+systemctl stop postgresql
+systemctl disable postgresql
